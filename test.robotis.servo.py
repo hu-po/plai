93c443e7621f477491ad05aae304a5840a848619
HugoCMU <hugo.p.cmu@gmail.com>
2023-02-14 16:03:28 -0600

    testing servos.

---
+++ test.robotis.servo.py
@@ -0,0 +1,15 @@
+"""
+
+From:
+
+https://emanual.robotis.com/docs/en/software/dynamixel/dynamixel_sdk/library_setup/python_linux/
+
+git clone https://github.com/ROBOTIS-GIT/DynamixelSDK.git    
+cd DynamixelSDK/python/
+pip install -e .
+sudo python setup.py install
+python DynamixelSDK/python/tests/protocol1_0/ping.py
+python DynamixelSDK/python/tests/protocol1_0/read_write.py
+
+
+"""
