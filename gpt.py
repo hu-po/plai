f9f1714b9de0577d51ac57a8c6bcfb98b2678473
hu.po <hugo.p.cmu@gmail.com>
2023-08-03 10:11:34 -0500

    gpt trajectories

--- gpt.py
+++ gpt.py
@@ -38,7 +38,7 @@ def gpt_text(
     temperature: float = DEFAULT_TEMPERATURE,
     max_tokens: int = DEFAULT_MAX_TOKENS,
     stop: List[str] = ["\n"],
-):
+) -> str:
     response: Dict = openai.ChatCompletion.create(
         messages=messages,
         model=model,
@@ -48,6 +48,60 @@ def gpt_text(
     )
     return response["choices"][0]["message"]["content"]
 
+def gpt_trajectory(
+    trajectory_description: str,
+    num_keyframes: int = 4,
+    model="gpt-3.5-turbo",
+    num_servos: int = 2,
+    keyframe_delimiter: str = ",",
+    servo_delimiter: str = " ",
+    min_value: int = 0,
+    max_value: int = 255,
+) -> List[Tuple[int, int]]:
+    _messages = [
+            {
+                "role": "system",
+                "content": " ".join(
+                    [
+                        "Given a description of a trajectory, output the trajectory.",
+                        f"You output trajectories for a {num_servos}DoF robot arm.",
+                        f"The trajectory is list of integers between {min_value} and {max_value}.",
+                        f"The delimiter used for the trajectory is {keyframe_delimiter}",
+                        f"The delimiter used for the servo values is {servo_delimiter}",
+                    ]
+                ),
+            },
+            {
+                "role": "user",
+                "content": "Give me a min, max, min trajectory with 3 keyframes.",
+            },
+            {
+                "role": "assistant",
+                "content": f"{servo_delimiter.join(['0'] * num_servos)}{keyframe_delimiter}{servo_delimiter.join(['255'] * num_servos)}{keyframe_delimiter}{servo_delimiter.join(['0'] * num_servos)}",
+            },
+            {
+                "role": "user",
+                "content": "Give me a blip trajectory with 2 keyframes.",
+            },
+            {
+                "role": "assistant",
+                "content": f"{servo_delimiter.join(['0'] * num_servos)}{keyframe_delimiter}{servo_delimiter.join(['10'] * num_servos)}",
+            },
+            {
+                "role": "user",
+                "content": f"Give me a {trajectory_description} trajectory with {num_keyframes} keyframes.",
+            },
+        ]
+    trajectory: List[Tuple[int, int]] = []
+    for keyframe in gpt_text(
+        messages=_messages,
+        model=model,
+    ).split(keyframe_delimiter):
+        if not keyframe:
+            continue
+        values = [_ for _ in keyframe.split(" ")]
+        trajectory.append((int(values[0]), int(values[1])))
+    return trajectory
 
 def multi_run(arg_sets):
     def decorator(func):
@@ -90,7 +144,6 @@ def gpt_test(
     max_value = 255,
     num_keyframes: int = 4,
 ):
-    set_openai_key()
     _messages = [
             {
                 "role": "system",
@@ -147,4 +200,12 @@ def gpt_test(
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.WARNING)
-    gpt_test()
+    set_openai_key()
+    # gpt_test()
+    print(gpt_trajectory("squiggle", num_keyframes=5))
+    print(gpt_trajectory("swoosh", num_keyframes=3))
+    print(gpt_trajectory("cosine", num_keyframes=40))
+    print(gpt_trajectory("zigzag", num_keyframes=7))
+    print(gpt_trajectory("spiral", num_keyframes=10))
+    print(gpt_trajectory("ellipse", num_keyframes=25))
+    print(gpt_trajectory("sinusoidal", num_keyframes=50))
