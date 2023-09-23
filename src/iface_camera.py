b1351077ad2fe00903b583c980f9103b6de66106
hu.po <hugo.p.cmu@gmail.com>
2023-09-23 14:45:54 -0500

    asdf

--- src/iface_camera.py
+++ src/iface_camera.py
@@ -4,21 +4,16 @@ from .camera import Camera
 
 log = logging.getLogger(__name__)
 
-class CameraCapture:
-    def __init__(self):
-        self.camera = Camera()
-
-    def capture(self, *args, **kwargs):
-        image = self.camera.get_image()
-        return image
-
-camera_capture = CameraCapture()
+def capture_video(self, *args, **kwargs):
+    camera = Camera()
+    video = camera.observe()  # This method returns a 4D numpy array
+    return video
 
 # Creating the interface
 iface_camera = gr.Interface(
-    fn=camera_capture.capture,
-    inputs=gr.Button(label='Capture Image'),
-    outputs=gr.Image(shape=(480, 640, 3)),
+    fn=capture_video,
+    inputs=gr.Button(label='Capture Video'),
+    outputs=gr.Video(shape=(camera.fpo, 480, 640, 3)),  # camera.fpo is the number of frames in the video
 )
 
 if __name__ == "__main__":
