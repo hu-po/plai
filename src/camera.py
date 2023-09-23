73392884d8b5264fe4e07e0841ad43b6c633796c
hu.po <hugo.p.cmu@gmail.com>
2023-09-23 13:20:42 -0500

    camera use ffmpeg

--- src/camera.py
+++ src/camera.py
@@ -1,14 +1,8 @@
 import numpy as np
 import logging
+import ffmpeg
 
 log = logging.getLogger(__name__)
-
-if 'cv2' not in globals():
-    try:
-        import cv2
-    except ImportError:
-        log.error("OpenCV is not installed. Please install it to use the Camera class.")
-        cv2 = None
 
 class Camera:
     def __init__(self, width: int = 224, height: int = 224, fps: int = 30):
@@ -19,13 +13,13 @@ class Camera:
         height (int): The height of the video frames.
         fps (int): The frames per second of the video.
         """
-        if cv2 is None:
-            raise RuntimeError("OpenCV is not installed. Please install it to use the Camera class.")
         log.info(f"Starting video capture at {width}x{height} {fps}fps")
-        self.cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-        self.cap.set(cv2.CAP_PROP_FPS, fps)
+        self.process = (
+            ffmpeg
+            .input('0', format='v4l2', video_size=f'{width}x{height}', framerate=fps)
+            .output('pipe:', format='rawvideo', pix_fmt='rgb24')
+            .run_async(pipe_stdout=True)
+        )
 
     def get_image(self) -> np.ndarray:
         """Capture a frame and return it as a numpy array.
@@ -36,23 +30,24 @@ class Camera:
         Raises:
         RuntimeError: If a frame capture fails.
         """
-        ret, image = self.cap.read()
-        if not ret:
+        raw_image = self.process.stdout.read(width*height*3)
+        if raw_image is None:
             log.error("Failed to capture frame")
             raise RuntimeError("Failed to capture frame")
-        # convert opencv output from BGR to RGB
-        image = image[:, :, [2, 1, 0]]
+        image = np.frombuffer(raw_image, np.uint8).reshape([height, width, 3])
         log.debug(f"Captured image {image.shape}")
         return image
 
     def __del__(self):
         """Releases the video capture when the object is deleted."""
         log.info("Ended video capture")
-        self.cap.release()
+        self.process.terminate()
 
 if __name__ == "__main__":
     camera = Camera()
     image = camera.get_image()
-    cv2.imshow("image", image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # Displaying the image requires a different approach as we're not using OpenCV anymore
+    # You can use matplotlib for example
+    import matplotlib.pyplot as plt
+    plt.imshow(image)
+    plt.show()
