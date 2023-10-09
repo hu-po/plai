ab3cf1e9cb40aa7394d4acc621a5bdc115f68379
hu.po <hugo.p.cmu@gmail.com>
2023-10-09 05:47:29 -0500

    sdk 1

--- src/gpt.py
+++ src/gpt.py
@@ -2,63 +2,41 @@ import logging
 import os
 from typing import Dict, List, Tuple
 
-import openai
-
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-KEYS_DIR = os.path.join(ROOT_DIR, ".keys")
-DEFAULT_LLM: str = "gpt-3.5-turbo"
-# DEFAULT_LLM: str = "gpt-4"
+from openai import OpenAI
 
 log = logging.getLogger(__name__)
 
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-def set_openai_key(key=None) -> str:
-    """
-    This function sets the OpenAI API key.
 
-    Parameters:
-    key (str): The OpenAI API key. If not provided, the function will try to read it from a file.
-
-    Returns:
-    str: The OpenAI API key.
-
-    Raises:
-    FileNotFoundError: If the key file is not found and no key is provided.
-    """
-    if key is None:
-        try:
-            # Try to read the key from a file
-            with open(os.path.join(KEYS_DIR, "openai.txt"), "r") as f:
-                key = f.read()
-        except FileNotFoundError:
-            log.warning("OpenAI API key not found.")
-            return
-    # Set the OpenAI API key
-    openai.api_key = key
-    log.info("OpenAI API key set.")
-    return key
+def set_openai_key():
+    if client.api_key is None:
+        log.warning("OpenAI API key not found.")
+    else:
+        log.info("OpenAI API key set.")
 
 
 def gpt_text(
     messages: List[Dict[str, str]] = None,
-    model=DEFAULT_LLM,
+    model="gpt-3.5-turbo",
     temperature: float = 0,
     max_tokens: int = 32,
     stop: List[str] = ["\n"],
 ) -> str:
     log.debug(f"Sending messages to OpenAI: {messages}")
-    response: Dict = openai.ChatCompletion.create(
+    response = client.chat_completions.create(
         messages=messages,
         model=model,
         temperature=temperature,
         max_tokens=max_tokens,
         stop=stop,
     )
-    reply: str = response["choices"][0]["message"]["content"]
+    reply: str = response.choices[0].message.content
     log.debug(f"Received reply from OpenAI: {reply}")
     return reply
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.WARNING)
     set_openai_key()
-    print(gpt_text(model="gpt-3.5-turbo", max_tokens=8, messages=[{"role": "user", "content": "hello"}]))
+    print(gpt_text(max_tokens=8, messages=[{"role": "user", "content": "hello"}]))
