2343524751b6dbd1d8664fda5eaaafdb10e2a6e1
hu-po <8493427+hu-po@users.noreply.github.com>
2023-10-29 17:38:56 -0500

    Update cam.py

--- src/cam.py
+++ src/cam.py
@@ -1,31 +1,40 @@
 import asyncio
 from ffmpeg.asyncio import FFmpeg
 from dataclasses import dataclass
+import os
 
 # Global variables for default values
 USERNAME_DEFAULT = "oop"
 REMOTE_IP_DEFAULT = "192.168.1.44"
+DEFAULT_DURATION = 3  # Default duration set to 3 seconds
 
 # Global variables for data paths
-PI_DATA_PATH = "/home/pi/dev/data/"
-OOP_DATA_PATH = "/home/oop/dev/data/"
+ROBOT_DATA_DIR = "/home/pi/dev/data/"
+REMOTE_DATA_DIR = "/home/oop/dev/data/"
 
 @dataclass
 class Camera:
-    Device: str
+    device: str
     name: str
     width: int
     height: int
     desc: str
 
+# Global variable for a list of Camera objects
+CAMERAS = [
+    Camera(device="/dev/video0", name="pi.stereo", width=960, height=1080, desc="Stereo Camera"),
+    Camera(device="/dev/video2", name="pi.mono", width=640, height=480, desc="Mono Camera"),
+]
+
 async def send_video(
-    directory_path: str = OOP_DATA_PATH,
+    robot_dir_path: str = ROBOT_DATA_DIR,
+    remote_dir_path: str = REMOTE_DATA_DIR,
     output_filename: str = "pi.stereo.mp4",
     username: str = USERNAME_DEFAULT,
     remote_ip: str = REMOTE_IP_DEFAULT,
 ) -> str:
-    local_path = PI_DATA_PATH + output_filename
-    remote_path = directory_path + output_filename
+    local_path = os.path.join(robot_dir_path, output_filename)
+    remote_path = os.path.join(remote_dir_path, output_filename)
     cmd = ["scp", local_path, f"{username}@{remote_ip}:{remote_path}"]
     process = await asyncio.create_subprocess_exec(
         *cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
@@ -40,15 +49,15 @@ async def send_video(
 
 async def record_video(
     camera: Camera,
-    duration: int
+    duration: int = DEFAULT_DURATION
 ) -> str:
-    output_path = PI_DATA_PATH + f"{camera.name}.mp4"
+    output_path = os.path.join(ROBOT_DATA_DIR, f"{camera.name}.mp4")
     ffmpeg = (
         FFmpeg()
         .option("y")
         .option("t", str(duration))
         .input(
-            camera.Device, format="v4l2",
+            camera.device, format="v4l2",
             framerate=camera.fps, video_size=f"{camera.width}x{camera.height}"
         )
         .output(output_path, vcodec="h264")
@@ -62,22 +71,12 @@ async def record_video(
         return f"Recording completed and saved to {output_path}"
 
 if __name__ == "__main__":
-    # Test case for stereo camera
-    stereo_camera = Camera(Device="/dev/video0", name="pi.stereo", width=960, height=1080, desc="Stereo Camera")
-    result_recording_stereo = asyncio.run(record_video(
-        camera=stereo_camera,
-        duration=2
-    ))
-    print(result_recording_stereo)
-    result_sending_stereo = asyncio.run(send_video())
-    print(result_sending_stereo)
-
-    # Test case for mono camera
-    mono_camera = Camera(Device="/dev/video2", name="pi.mono", width=640, height=480, desc="Mono Camera")
-    result_recording_mono = asyncio.run(record_video(
-        camera=mono_camera,
-        duration=2
-    ))
-    print(result_recording_mono)
-    result_sending_mono = asyncio.run(send_video(directory_path=OOP_DATA_PATH))
-    print(result_sending_mono)
+    # Iterate through the camera objects and make AsyncIO calls
+    for camera in CAMERAS:
+        result_recording = asyncio.run(record_video(
+            camera=camera,
+            duration=2  # Override default duration for this test case
+        ))
+        print(result_recording)
+        result_sending = asyncio.run(send_video())
+        print(result_sending)
