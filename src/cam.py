3981bef27b39cfcfe45ba2aab7d4b67a764614ad
hu-po <8493427+hu-po@users.noreply.github.com>
2023-10-29 17:25:15 -0500

    Update cam.py

--- src/cam.py
+++ src/cam.py
@@ -1,13 +1,31 @@
 import asyncio
 from ffmpeg.asyncio import FFmpeg
+from dataclasses import dataclass
 
+# Global variables for default values
+USERNAME_DEFAULT = "oop"
+REMOTE_IP_DEFAULT = "192.168.1.44"
+
+# Global variables for data paths
+PI_DATA_PATH = "/home/pi/dev/data/"
+OOP_DATA_PATH = "/home/oop/dev/data/"
+
+@dataclass
+class Camera:
+    Device: str
+    name: str
+    width: int
+    height: int
+    desc: str
 
 async def send_video(
-    local_path: str,
-    remote_path: str,
-    username: str = "oop",
-    remote_ip: str = "192.168.1.44",
+    directory_path: str = OOP_DATA_PATH,
+    output_filename: str = "pi.stereo.mp4",
+    username: str = USERNAME_DEFAULT,
+    remote_ip: str = REMOTE_IP_DEFAULT,
 ) -> str:
+    local_path = PI_DATA_PATH + output_filename
+    remote_path = directory_path + output_filename
     cmd = ["scp", local_path, f"{username}@{remote_ip}:{remote_path}"]
     process = await asyncio.create_subprocess_exec(
         *cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
@@ -20,21 +38,18 @@ async def send_video(
     else:
         return f"Successfully sent {local_path} to {remote_ip}:{remote_path}"
 
-
 async def record_video(
-    output_path: str,
-    width: int,
-    height: int,
-    fps: int,
-    video_device: str,
+    camera: Camera,
     duration: int
 ) -> str:
+    output_path = PI_DATA_PATH + f"{camera.name}.mp4"
     ffmpeg = (
         FFmpeg()
         .option("y")
         .option("t", str(duration))
         .input(
-            video_device, format="v4l2", framerate=fps, video_size=f"{width}x{height}"
+            camera.Device, format="v4l2",
+            framerate=camera.fps, video_size=f"{camera.width}x{camera.height}"
         )
         .output(output_path, vcodec="h264")
     )
@@ -46,36 +61,23 @@ async def record_video(
     else:
         return f"Recording completed and saved to {output_path}"
 
-
 if __name__ == "__main__":
     # Test case for stereo camera
+    stereo_camera = Camera(Device="/dev/video0", name="pi.stereo", width=960, height=1080, desc="Stereo Camera")
     result_recording_stereo = asyncio.run(record_video(
-        output_path="/home/pi/dev/data/pi.stereo.mp4",
-        width=960,
-        height=1080,
-        fps=5,
-        video_device="/dev/video0",
+        camera=stereo_camera,
         duration=2
     ))
     print(result_recording_stereo)
-    result_sending_stereo = asyncio.run(send_video(
-        local_path="/home/pi/dev/data/pi.stereo.mp4",
-        remote_path="/home/oop/dev/data/pi.stereo.mp4"
-    ))
+    result_sending_stereo = asyncio.run(send_video())
     print(result_sending_stereo)
 
     # Test case for mono camera
+    mono_camera = Camera(Device="/dev/video2", name="pi.mono", width=640, height=480, desc="Mono Camera")
     result_recording_mono = asyncio.run(record_video(
-        output_path="/home/pi/dev/data/pi.mono.mp4",
-        width=640,
-        height=480,
-        fps=5,
-        video_device="/dev/video2",
+        camera=mono_camera,
         duration=2
     ))
     print(result_recording_mono)
-    result_sending_mono = asyncio.run(send_video(
-        local_path="/home/pi/dev/data/pi.mono.mp4",
-        remote_path="/home/oop/dev/data/pi.mono.mp4"
-    ))
+    result_sending_mono = asyncio.run(send_video(directory_path=OOP_DATA_PATH))
     print(result_sending_mono)
